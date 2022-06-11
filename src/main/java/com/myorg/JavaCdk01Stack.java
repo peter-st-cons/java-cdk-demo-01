01bf80a9f0c4ea7a6cb544b93d564fe4f7f6401c
Peter Störmer <mail@peter-stoermer.de>
2022-06-11 16:04:08 +0200

    feat: upgrade to cdk 2.27.0

--- src/main/java/com/myorg/JavaCdk01Stack.java
+++ src/main/java/com/myorg/JavaCdk01Stack.java
@@ -1,17 +1,17 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Stack;
-import software.amazon.awscdk.core.StackProps;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
 
 public class JavaCdk01Stack extends Stack {
-    public JavaCdk01Stack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+  public JavaCdk01Stack(final Construct scope, final String id) {
+    this(scope, id, null);
+  }
 
-    public JavaCdk01Stack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+  public JavaCdk01Stack(final Construct scope, final String id, final StackProps props) {
+    super(scope, id, props);
 
-        // The code that defines your stack goes here
-    }
+    // The code that defines your stack goes here
+  }
 }
--- src/test/java/com/myorg/JavaCdk01Test.java
+++ src/test/java/com/myorg/JavaCdk01Test.java
@@ -1,6 +1,6 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.App;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -11,18 +11,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaCdk01Test {
-    private final static ObjectMapper JSON =
-        new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
+  private final static ObjectMapper JSON = new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
 
-    @Test
-    public void testStack() throws IOException {
-        App app = new App();
-        JavaCdk01Stack stack = new JavaCdk01Stack(app, "test");
+  @Test
+  public void testStack() throws IOException {
+    App app = new App();
+    JavaCdk01Stack stack = new JavaCdk01Stack(app, "test");
 
-        // synthesize the stack to a CloudFormation template
-        JsonNode actual = JSON.valueToTree(app.synth().getStackArtifact(stack.getArtifactId()).getTemplate());
+    // synthesize the stack to a CloudFormation template
+    JsonNode actual = JSON.valueToTree(app.synth().getStackArtifact(stack.getArtifactId()).getTemplate());
 
-        // Update once resources have been added to the stack
-        assertThat(actual.get("Resources")).isNull();
-    }
+    // Update once resources have been added to the stack
+    assertThat(actual.get("Resources")).isNull();
+  }
 }
